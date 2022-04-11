fe1251419715423fb274d11c9e52caa79df06509
Jason I Chavez <jason.chavez@lmco.com>
2022-04-11 08:30:45 -0400

    Final update

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    // p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
     
+    // Calculate interest
+    var interest = principal * years * rate/100;
+    
+    // Get the final year in the future
+    var year = new Date().getFullYear()+parseInt(years);
+
+    // Display the results
+    document.getElementById("result").innerHTML="If you deposit "+
+    "<span class='highlight'>"+principal+"</span>"+
+    ",\<br\>at an interest rate of "+
+    "<span class='highlight'>"+rate+"</span>"+
+    "%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+    
+}
+
+// Function to read the range slider value and display it in the span
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
 }
         
